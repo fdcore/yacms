67a10ff8acc86c4403a571d28925199028e7398c
Дмитрий Няшкин <admin@fdcore.com>
2016-02-01 11:08:52 +0300

    fixes

--- src/_app/admin_routes.php
+++ src/_app/admin_routes.php
@@ -1,21 +1,25 @@
 <?php
 
-$controllers = directory_map('_app/controllers/admin');
+$controllers = directory_map(BASE_PATH . '_app/controllers/admin');
 
-foreach ($controllers as $file) {
-    $file = mb_pathinfo($file);
-    $file = $file['filename'];
+if(count($controllers) > 0) {
+	foreach ($controllers as $file) {
+	    $file = mb_pathinfo($file);
+	    $file = $file['filename'];
 
-    if($file == 'index') continue;
+	    if($file == 'index') continue;
 
-    $app->group('/admin/'.$file, function () use($app, $file) {
-        include_once '_app/controllers/admin/'.$file.'.php';
-    });
+	    $app->group('/admin/'.$file, function () use($app, $file) {
+	        include_once BASE_PATH . '_app/controllers/admin/'.$file.'.php';
+	    });
+	}
 }
 
 
-if(file_exists(BASE_PATH.'/_app/controllers/admin/index.php')){
+if(file_exists(BASE_PATH . '_app/controllers/admin/index.php')){
+
     $app->group('/admin', function () use($app) {
-        include_once '_app/controllers/admin/index.php';
+        include_once BASE_PATH . '_app/controllers/admin/index.php';
     });
+
 }
